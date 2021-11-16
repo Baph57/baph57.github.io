f2806c66b99e89f0e852ce353467837a9b493d48
Baph57 <hybridcrayon@yahoo.com>
2021-11-15 18:45:21 -0700

    final project initialized and added

---
+++ scripts.js
@@ -0,0 +1,16 @@
+//insert text into a div using innerHTML
+var fill = document.getElementById("homebanner");
+homebanner.innerHTML = "SNHU-PALOOZA!";
+
+var description_one = document.getElementById("photo_one");
+description_one.innerHTML = "Yeah, why not? 'Cause it's just pure. Pure and complete gnarlyisms. You know, he might be nails, but I'm frickin bayonets, you know? I am battle-tested bayonets, bro."
+
+var description_two = document.getElementById("photo_two");
+description_two.innerText = "But ... whatever. I agreed to do it. There's just one deal point that Jim Robinson - a wonderful man at Morgan Creek, a great company - need to work out is they want me in it. If they do, it's a smash. If they don't, it's a turd that opens on a tugboat.  Well, yeah. But I was gonna tell you, I'm tired of being told like, 'Oh, you can't talk about that. You can't talk about that."
+
+var description_three = document.getElementById("photo_three");
+photo_three.innerHTML = "I am the Doctor, and you are the Daleks! Did I mention we have comfy chairs? Father Christmas. Santa Claus. Or as I've always known him: Jeff. You hit me with a cricket bat. I'm the Doctor. Well, they call me the Doctor. __I don't know why.__ *I call me the Doctor too.* I still don't know why."
+
+var description_four = document.getElementById("photo_four");
+photo_four.innerHTML = "They're not aliens, they're Earth…liens! You've swallowed a planet! Saving the world with meals on wheels. Annihilate? No. No violence. I won't stand for it. Not now, not ever, do you understand me?! I'm the Doctor, the Oncoming Storm - and you basically meant beat them in a football match, didn't you?"
+
